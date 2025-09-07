332b509c4ead796b52615c95376466a885b82d67
Ghuilherme29 <guilherme.silva15@ba.estudante.senai.br>
2025-09-06 21:06:13 -0300

    Update questão8.py

--- questão8.py
+++ questão8.py
@@ -1,20 +1,28 @@
 import os
-os.system
+os.system('cls')
 
 
-precos = {
-    'VERDE': 10.00,
-    'AZUL': 20.00,
-    'AMARELO': 30.00,
-    'VERMELHO': 40.00
-}
+print('''
+  Bem-vindo a loja de CDs!
+  Temos as seguintes opções de cores para os CDs:
+          
+    |   cor    |   preço 
+    |  Verde   |  R$ 10,00 
+    |  Azul    |  R$ 20,00
+    |  Amarelo |  R$ 30,00
+    |  Vermelho|  R$ 40,00 \n''')
 
-cor = input("Digite a cor dos CDs (VERDE, AZUL, AMARELO, VERMELHO): ").upper()
+cor = input('Escolha a cor dos CDs desejada:').upper()
 
-
-if cor in precos:
-    preco_selecionado = precos[cor]
-    print(f"O preço do CD da cor {cor} é: R$ {preco_selecionado:.2f}")
-else:
-   
-    print("Cor inválida. Por favor, escolha entre VERDE, AZUL, AMARELO ou VERMELHO.")
+  
+match cor:
+    case "VERDE":
+        print('Você escolheu a cor (verde:), o preço é R$ 10,00')
+    case "AZUL":
+        print('Você escolheu a cor azul, o preço é R$ 20,00')
+    case "AMARELO":
+        print('Você escolheu a cor amarelo, o preço é R$ 30,00')
+    case "VERMELHO":
+        print('Você escolheu a cor vermelho, o preço é R$ 40,00')
+    case _:
+        print('Opção inválida, tente novamente!')
